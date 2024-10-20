5563ce585e4c93f5f60ea75890e94586e30f54d2
Ahmad Fathoni <ahmad.fathoni50@gmail.com>
2024-10-20 13:22:02 +0700

    ✅ redirect to home if authenticated in login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,15 @@
 import { Toaster } from 'react-hot-toast';
 import LoginContent from '../components/pages/LoginContent';
+import useLocalStorage from '../hooks/useLocalStorage';
+import { Navigate } from 'react-router-dom';
 
 function LoginPage() {
+  const { accessToken } = useLocalStorage();
+
+  if (accessToken) {
+    return <Navigate to={'/'} />;
+  }
+
   return (
     <>
       <Toaster />
