f272aa6d7a3a4f09c705b6ede0586894842d327c
Ahmad Fathoni <ahmad.fathoni50@gmail.com>
2024-10-20 12:43:43 +0700

    ✅ add login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import { Toaster } from 'react-hot-toast';
+import LoginContent from '../components/pages/LoginContent';
+
+function LoginPage() {
+  return (
+    <>
+      <Toaster />
+      <LoginContent />
+    </>
+  );
+}
+
+export default LoginPage;
